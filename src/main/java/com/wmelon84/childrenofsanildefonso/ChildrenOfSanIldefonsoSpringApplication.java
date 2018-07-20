ff52d889069f6faaa249e557fea602251a06a3b2
Avelino <avelinoc@softcom.com>
2018-07-20 14:08:43 +0200

    Add project base

---
+++ src/main/java/com/wmelon84/childrenofsanildefonso/ChildrenOfSanIldefonsoSpringApplication.java
@@ -0,0 +1,12 @@
+package com.wmelon84.childrenofsanildefonso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChildrenOfSanIldefonsoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChildrenOfSanIldefonsoSpringApplication.class, args);
+	}
+}
---
+++ src/test/java/com/wmelon84/childrenofsanildefonso/ChildrenOfSanIldefonsoSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wmelon84.childrenofsanildefonso;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChildrenOfSanIldefonsoSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
